0fa6889e8a325491eaf7ffa5bfeb7b5e4206713d
Adair <111542919+Adairowo@users.noreply.github.com>
2025-09-02 15:52:53 -0700

    adasdasd

---
+++ ACME/database/migrations/2025_09_02_213949_create_clientes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_cliente', 150);
+            $table->string('direccion_contacto', 200)->nullable();
+            $table->string('telefono', 20)->nullable();
+            $table->string('email', 120)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
---
+++ ACME/database/migrations/2025_09_02_213955_create_proyectos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->string('nombre_proyecto', 150);
+            $table->string('direccion_proyecto', 200)->nullable();
+            $table->foreignId('zona_id')->constrained('zonas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
