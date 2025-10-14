b68d7699abc749a6727304463ab628841aaf7ceb
Adair <111542919+Adairowo@users.noreply.github.com>
2025-10-14 15:50:59 -0700

    Se añadio plantilla dashboard

---
+++ ACME/resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.head')
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @include('layouts.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('layouts.topbar')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    @yield('content')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            @include('layouts.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    @include('layouts.scripts')
+
+</body>
+
+</html>
--- ACME/routes/web.php
+++ ACME/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\ZonasController;
+use App\Http\Controllers\ProyectosController;
+use App\Http\Controllers\PlanosController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('clientes', ClientesController::class);
+Route::resource('zonas', ZonasController::class);
+Route::resource('proyectos', ProyectosController::class);
+Route::resource('planos', PlanosController::class);
